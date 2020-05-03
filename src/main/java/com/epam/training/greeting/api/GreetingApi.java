faf33056d42a6242a17676b656d27ac9148825c4
Oleg Hudyma <oleg.hudyma@aerlingus.com>
2020-05-03 16:27:28 +0300

    Added k8s configuration + logs

--- src/main/java/com/epam/training/greeting/api/GreetingApi.java
+++ src/main/java/com/epam/training/greeting/api/GreetingApi.java
@@ -3,6 +3,7 @@ package com.epam.training.greeting.api;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
+import java.net.UnknownHostException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,5 +15,5 @@ public interface GreetingApi {
   @GetMapping
   @ApiOperation(value = "Greet")
   @ApiResponse(code = 200, message = "OK", response = String.class)
-  String greet(@RequestParam(defaultValue = "Anonymous") String name);
+  String greet(@RequestParam(defaultValue = "Anonymous") String name) throws UnknownHostException;
 }
