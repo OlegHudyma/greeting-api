0a6b19887ddacb273042ed209fd13d3df7bc1792
Oleg Hudyma <oleg_hudyma@epam.com>
2021-05-27 17:38:25 +0300

    Cleaned up

--- src/test/java/com/epam/training/greeting/GreetingApiApplicationTests.java
+++ src/test/java/com/epam/training/greeting/GreetingApiApplicationTests.java
@@ -1,10 +1,8 @@
 package com.epam.training.greeting;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Ignore
 @SpringBootTest
 public class GreetingApiApplicationTests {
 
